0b32b407b83416db768f4adbda90a02496d235b7
Perplexity Computer <perplexity@broyhillgop.com>
2026-03-29 18:59:16 +0000

    fix_08 Pre-Step C chunks 11-14: Fast version using ILIKE instead of ~* regex for addr_type classification

---
+++ database/fix_scripts/fix08_prestep_c_fast/chunk_14_fast.sql
@@ -0,0 +1,29 @@
+-- fix_08 Pre-Step C chunk 14/14 (FAST version)
+-- Uses ILIKE + regex ^ anchor instead of full ~* regex
+-- rncid range: 24243127230 to 24243709909
+
+SET statement_timeout = 0;
+UPDATE nc_datatrust SET
+  norm_first = UPPER(TRIM(firstname)),
+  norm_last  = UPPER(TRIM(lastname)),
+  norm_zip5  = LEFT(regzip5, 5),
+  addr_type  = CASE
+    WHEN registrationaddr1 ILIKE 'PO BOX%'
+      OR registrationaddr1 ILIKE 'P.O. BOX%'
+      OR registrationaddr1 ILIKE 'P O BOX%'
+      OR registrationaddr1 ILIKE 'POB %'    THEN 'po_box'
+    WHEN registrationaddr1 ILIKE 'RR %'
+      OR registrationaddr1 ILIKE 'R.R.%'    THEN 'rural_route'
+    WHEN registrationaddr1 ILIKE 'HC %'     THEN 'highway_contract'
+    WHEN registrationaddr1 ~ '^[0-9]'       THEN 'street'
+    ELSE 'unknown' END,
+  norm_street_num = CASE
+    WHEN registrationaddr1 ILIKE 'PO BOX%'
+      OR registrationaddr1 ILIKE 'P.O. BOX%'
+      OR registrationaddr1 ILIKE 'P O BOX%'
+      THEN SUBSTRING(registrationaddr1 FROM '[0-9]+')
+    WHEN registrationaddr1 ~ '^[0-9]'
+      THEN SUBSTRING(registrationaddr1 FROM '^([0-9]+)')
+    ELSE NULL END
+WHERE rncid::bigint >= 24243127230 AND rncid::bigint < 24243709909
+  AND norm_first IS NULL;
